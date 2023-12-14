af4ad588d1ed0f712e0d0e891f0ebc94710dbc4e
BTejesh27 <btejesh_csd@srkrec.edu.in>
2023-12-14 15:21:13 +0530

    tej

--- dist/admin.php
+++ dist/admin.php
@@ -28,69 +28,83 @@ if (isset($_POST['upload'])) {
     <link rel="stylesheet" href="style.css">
     <title>Tennis Tournament Admin</title>
     <style>
-        .body {
-            font-family: 'Arial', sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f4f4f4;
-        }
+    .body {
+        font-family: 'Arial', sans-serif;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #f4f4f4;
+    }
 
-        .login-container {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
+    .login-container {
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        width: 300px; /* Set a fixed width if desired */
+        text-align: center; /* Center text content */
+    }
 
-        form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
-        h2 {
-            text-align: center;
-            color: #333;
-        }
+    h2 {
+        text-align: center;
+        color: #333;
+    }
 
-        label {
-            margin-bottom: 8px;
-            color: #555;
-        }
+    label {
+        margin-bottom: 8px;
+        color: #555;
+    }
 
+    input {
+        padding: 10px;
+        margin-bottom: 16px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        width: 100%;
+        box-sizing: border-box;
+    }
 
-        input {
-            padding: 10px;
-            margin-bottom: 16px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            width: 100%;
-            box-sizing: border-box;
-        }
+    button {
+        padding: 12px;
+        background-color: #3498db;
+        color: #fff;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        width: 100%;
+        box-sizing: border-box;
+    }
 
-        button {
-            padding: 12px;
-            background-color: #3498db;
-            color: #fff;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            width: 100%;
-            box-sizing: border-box;
-        }
+    button:hover {
+        background-color: #2980b9;
+    }
+    .login-container {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        width: 300px; /* Set a fixed width if desired */
+        text-align: center; /* Center text content */
+    }
+</style>
 
-        button:hover {
-            background-color: #2980b9;
-        }
-    </style>
 </head>
 
 <body>
-    <div>
+    
 
         <div class="login-container">
             <h2>Admin Login</h2>
@@ -106,7 +120,7 @@ if (isset($_POST['upload'])) {
                 <button type="submit" name="upload">Login</button>
             </form>
         </div>
-    </div>
+    
 </body>
 
 </html>
