7a7b38007c2c9817e726ea5844d1dd20ce41ec7d
Abhi200 <saiabhinaygedela@gmail.com>
2024-01-23 09:29:35 +0530

    index

--- dist/index.php
+++ dist/index.php
@@ -86,7 +86,7 @@
                     <!-- Sidebar Toggle-->
                     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
 
-                </nav>
+                </nav><h1>hiiiii</h1>
                 <div id="layoutSidenav">
                     <div id="layoutSidenav_nav">
                         <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
