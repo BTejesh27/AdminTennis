7303ad96e1253637e0ee79fbacf6d49abd3c6a37
BTejesh27 <btejesh_csd@srkrec.edu.in>
2024-02-06 10:33:54 +0530

    nav

--- dist/nav.php
+++ dist/nav.php
@@ -105,7 +105,7 @@ $userPassword = $_SESSION['user_password'];
                             <!-- <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div> -->
                             Update doubles
                         </a>
-                        <a class="nav-link" href="updateplayer_points.php">
+                        <a class="nav-link" href="player_points.php">
                             <!-- <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div> -->
                             Update Player points
                         </a>
